4259c3bfeb6385bea9e351882904674b189990ff
Sergey Kryakin <s.kryakin@informunity.ru>
2022-05-31 08:50:30 +0300

    log

--- ami.go
+++ ami.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"log"
 	"net"
 	"strconv"
 	"sync"
@@ -69,6 +70,7 @@ func newAMIAdapter(s *Settings, eventEmitter func(string, string)) (*amiAdapter,
 				break
 			}
 			func() {
+				log.Println(a.reconnect)
 				if !a.reconnect {
 					return
 				}
